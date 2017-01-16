b15dc22e44cc8b5efe66a80e79c98a86ae897d6d
Arif <dharif23@gmail.com>
2017-01-16 21:15:23 +0100

    database operations for processing data

---
+++ dbOperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin python3 -tt
+# Character_encoding: UTF-8
+# -------------------------------------------------------------
+# Author: Dalwar Hossain (dalwar014@gmail.com)
+# Python Interpreter: >= 3.4
+# mySQL Database Version: 5.6.28
+# mysql Connector Version: 2.1.5
+# -------------------------------------------------------------
+# Import custom library functions
+import primaryTableCleaner
+import importCsv2Db
+# Create a function to execute the threads
+def executeThreads(dbConnection,csvFileLocation):
+	# Clean the Primary Tables to insert data
+	primaryTableCleaner.truncate(dbConnection)
+	# Insert data from file to database
+	importCsv2Db.csvImport(dbConnection,csvFileLocation)
+# -------------------------------------------------------------
+# This is a standard boilerplate that calls the main() function
+if __name__ == '__main__':
+	main()
